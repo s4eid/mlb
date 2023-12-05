c3934324c0645908245ff308fa6e49812e90c7ea
saeid <saeid.noormohammad@gmail.com>
2023-12-05 11:51:37 +0300

    main

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
       {/* <h1>Lets see how its look</h1> */}
       <div className={projects.mainC}>
         <div className={projects.mainHolder}>
-          <Link href={"/products/hybrid/penguin-hybrid"}>
+          <Link href={"/products/hybrid/pigyt"}>
             <Image src={"/project-2.jpg"} width={600} height={480} />
             <h3>Hybrid</h3>
           </Link>
@@ -17,7 +17,7 @@ const Projects = () => {
         <div className={projects.holder}>
           <div className={projects.inner}>
             <div className={projects.mainHolderL}>
-              <Link href={"/products/timber/australian-timber"}>
+              <Link href={"/products/timber/aus-timber"}>
                 <Image src={"/timber.jpg"} width={300} height={230} />
                 <h3>Timber</h3>
               </Link>
@@ -25,13 +25,13 @@ const Projects = () => {
           </div>
           <div className={projects.inner}>
             <div className={projects.mainHolderL}>
-              <Link href={"/products/carpet/refined"}>
+              <Link href={"/products/carpet/syfined"}>
                 <Image src={"/carpet.jpg"} width={300} height={230} />
                 <h3>Carpet</h3>
               </Link>
             </div>
             <div className={projects.mainHolderL}>
-              <Link href={"/products/laminate/fusion"}>
+              <Link href={"/products/laminate/fisiyon"}>
                 <Image src={"/project-5.jpg"} width={300} height={230} />
                 <h3>Laminate</h3>
               </Link>
